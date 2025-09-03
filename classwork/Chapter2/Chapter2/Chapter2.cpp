1d9dc98f68c0fc0de5d4ca83ae6439807acc1a48
Christopher Brink <christopher.brink@my.tccd.edu>
2025-09-02 21:31:21 -0500

    Chapter 2 In-Class Practice

---
+++ classwork/Chapter2/Chapter2/Chapter2.cpp
@@ -0,0 +1,15 @@
+/*
+* Chris
+* Lab 1
+* Cosc 1436 Fall 2025
+*/
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World " << "" << "Chris\n";
+    std::cout << "You are " << 20 << " years old" << std::endl;
+    std::string name;
+    name = "Chris";
+    std::cout << "Hello World " << name;
+}
