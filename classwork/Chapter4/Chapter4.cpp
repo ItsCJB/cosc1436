278c63ad9058e1b340b805cae414ed0845e6b16d
Christopher Brink <christopher.brink@my.tccd.edu>
2025-09-25 18:31:52 -0500

    lab1

---
+++ classwork/Chapter4/Chapter4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<string>
+
+int main()
+{
+    //movie details
+std:string title;    //required
+std:string description //optional
+int runLength;        // required, 0 
+int releaseYear;    //optional,but between 1900-2100
+double userRating;  // optional, 1.0-10.0
+bool isClassic;     //required, false 
+
+// Get movie details
+std::cout << "Enter movie title: ";
+std::getline(std::cin, title);
+
+//Title is required 
+bool is Empty = title == "";
+if (isEmpty)
+
+}
